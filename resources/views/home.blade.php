dfad809aabcf1345f6188fb0d3a02489fdb7d7c3
Joel Aduma <caesaragen@gmail.com>
2022-02-15 13:08:56 +0300

    add changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+        <div class="col-md-3 p-5">
+            <img src="https://img.freepik.com/free-psd/digital-mobile-phone-screen-mockup_23-2149196260.jpg?w=826" class="header-img" alt="">
+        </div>
+        <div class="col-md-9 pt-5">
+            <div>
+                <h1>IGClone</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>154</strong>Posts</div>
+                <div class="pr-5"><strong>1200</strong>Followers</div>
+                <div class="pr-5"><strong>204</strong>Following</div>
             </div>
         </div>
     </div>
